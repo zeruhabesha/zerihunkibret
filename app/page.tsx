1bea67a6fbce3d613a5a6c41fad6935eadd5187b
Zerihun Kibret <88440051+zeruhabesha@users.noreply.github.com>
2025-07-04 09:40:40 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -61,7 +61,7 @@ export default function Home() {
       description: "Website for Ably Electric showcasing electrical services and products.",
       technologies: ["PHP", "CSS", "JavaScript", "MySQL"],
       image: "/images/ably.png",
-      link: "https://ablyelectric.vercel.app",
+      link: "https://ablyelectric.com",
     },
     
      {
